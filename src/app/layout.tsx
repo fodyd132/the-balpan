f2adf387934063f812abce1cdbf41e29b849beb3
fodyd132 <kyr6533@naver.com>
2025-07-17 10:56:00 +0900

    fix: layout에 Footer 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import Header from '@/app/components/Header'
+import Footer from './components/Footer'
 
 export const metadata: Metadata = {
   title: '더 발판',
@@ -12,7 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko">
       <body className="bg-white text-gray-900">
         <Header />
-        {children}
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </body>
     </html>
   )
